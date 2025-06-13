1adcf5389b4fd2573e24a5dc34d2ba8bd259beef
angel <casanosfera@gmail.com>
2025-06-13 15:16:23 -0600

    agregado vista

---
+++ vista.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Caracteristica</title>
+</head>
+<body>
+
+<h1>Agregando nueva caracteristica</h1>
+    
+</body>
+</html>
